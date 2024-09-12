c6784bea567e62bb5dea40ee57b489b4d08e2f9a
Nicole Drakes <bfwqi5qp@students.codeinstitute.net>
2024-09-12 13:35:23 +0000

    Initialize python project and create battleships game board setup

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def initialize_board():
+    return [['O' for _ in range(5)] for _ in range(5)]
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
